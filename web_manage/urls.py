36c16b604e7f44e8608af2621112c064c5c47919
richie <lmdkfs@126.com>
2015-12-19 22:16:44 +0800

    add models and  urls

---
+++ web_manage/urls.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+__author__ = 'Richie'
+from django.conf.urls import include, url
+from web_manage.views import home
+urlpatterns = [
+    url(r'', home.index),
+]
