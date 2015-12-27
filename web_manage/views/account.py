ce2963223834a379c89effbd2e8e89f35b75d467
ZhuRuiQing <lmdkfs@aliyun.com>
2015-12-27 23:49:50 +0800

    modify index.html:  input div  and so so

---
+++ web_manage/views/account.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*-coding: utf-8 -*-
+# __author__ = 'Richie'
+import StringIO
+from django.shortcuts import render_to_response
+from django.shortcuts import HttpResponse
+from django.shortcuts import redirect
