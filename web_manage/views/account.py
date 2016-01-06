490b453aaf88715824cacb68bef124a6e0eabad0
zrq <lmdkfs@126.com>
2016-01-06 13:10:42 +0800

    modify  login.html and add view - account

--- web_manage/views/account.py
+++ web_manage/views/account.py
@@ -5,3 +5,14 @@ import StringIO
 from django.shortcuts import render_to_response
 from django.shortcuts import HttpResponse
 from django.shortcuts import redirect
+from backend.commons.check_code import create_validate_code
+from web_manage.forms.account import LoginForm
+
+def check_code(request):
+    stream_obj = StringIO.StringIO()
+    validate_code = create_validate_code()
+    img = validate_code[0]
+    img.save(stream_obj, 'GIF')
+    request.session['CheckCode'] = validate_code[1]
+    print stream_obj
+    return HttpResponse(stream_obj.getvalue())
